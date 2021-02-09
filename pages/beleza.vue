6b997044561ca2f02a5322f337b4d07ec5c4cfd5
gustavoGarciaPereira <gusgurtavo@gmail.com>
2021-02-09 12:44:14 -0300

    primeiro deploy

---
+++ pages/beleza.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>belezinha</h1>
+
+        <NuxtLink to='/'>
+        home
+        </NuxtLink>
+        
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>gustavo</h1>
+        <NuxtLink to='/beleza'>
+            beleza
+        </NuxtLink>
+    </div>
+</template>
