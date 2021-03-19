323d7ba4deb20d48bc9792e6789185d7fbb78223
gustavoGarciaPereira <gusgurtavo@gmail.com>
2021-03-19 16:27:26 -0300

    nuxt js, codigos

--- pages/js-legal.vue
+++ pages/js-legal.vue
@@ -8,7 +8,8 @@
         </div>
 
 
-        <p>o que aprendi hoje com java scrit</p>
+        <h2>o que aprendi hoje com javascrit</h2>
+        <h3>adicionar no array</h3>
         <p>modificar arrays</p>
         <p>para adicionar um elemento no ultimo lugar do array</p>
         <p>temos o metodo push</p>
@@ -47,8 +48,31 @@
 
 
 
-<p>temos ainda os metodos de remoção</p>
-<p>o uso mais basico é não passando</p>
+<h3>temos ainda os metodos de remoção</h3>
+<p>o uso mais basico é não passando nenhum parametro já que estavamos</p>
+<p>removando de um array pre existente</p>
+<p>primeiro com o metodo pop</p>
+
+<pre style="color:#55cc66;background:#001800;"><span style="color:#808030; ">&gt;</span> array
+<span style="color:#808030; ">[</span> <span style="color:#800000; ">'</span><span style="color:#cc5555; ">gu</span><span style="color:#800000; ">'</span><span style="color:#808030; ">,</span> <span style="color:#800000; ">'</span><span style="color:#cc5555; ">gu</span><span style="color:#800000; ">'</span><span style="color:#808030; ">,</span> <span style="color:#800000; ">'</span><span style="color:#cc5555; ">gu</span><span style="color:#800000; ">'</span><span style="color:#808030; ">,</span> <span style="color:#800000; ">'</span><span style="color:#cc5555; ">ultimo</span><span style="color:#800000; ">'</span><span style="color:#808030; ">,</span> <span style="color:#800000; ">'</span><span style="color:#cc5555; ">novo ultomo</span><span style="color:#800000; ">'</span><span style="color:#808030; ">,</span> <span style="color:#800000; ">'</span><span style="color:#cc5555; ">novo ultomo</span><span style="color:#800000; ">'</span> <span style="color:#808030; ">]</span>
+<span style="color:#808030; ">&gt;</span> 
+<span style="color:#808030; ">&gt;</span> array<span style="color:#808030; ">.</span>pop<span style="color:#808030; ">(</span><span style="color:#808030; ">)</span>
+<span style="color:#800000; ">'</span><span style="color:#cc5555; ">novo ultomo</span><span style="color:#800000; ">'</span>
+<span style="color:#808030; ">&gt;</span> 
+</pre>
+
+
+<p>e o metodo que remove o primeiro</p>
+<p>metodo shift</p>
+
+<pre style="color:#55cc66;background:#001800;"><span style="color:#808030; ">&gt;</span> array
+<span style="color:#808030; ">[</span> <span style="color:#800000; ">'</span><span style="color:#cc5555; ">gu</span><span style="color:#800000; ">'</span><span style="color:#808030; ">,</span> <span style="color:#800000; ">'</span><span style="color:#cc5555; ">gu</span><span style="color:#800000; ">'</span><span style="color:#808030; ">,</span> <span style="color:#800000; ">'</span><span style="color:#cc5555; ">gu</span><span style="color:#800000; ">'</span><span style="color:#808030; ">,</span> <span style="color:#800000; ">'</span><span style="color:#cc5555; ">ultimo</span><span style="color:#800000; ">'</span> <span style="color:#808030; ">]</span>
+<span style="color:#808030; ">&gt;</span> array<span style="color:#808030; ">.</span><span style="color:#508050; font-weight:bold; ">shift</span><span style="color:#808030; ">(</span><span style="color:#808030; ">)</span>
+<span style="color:#800000; ">'</span><span style="color:#cc5555; ">gu</span><span style="color:#800000; ">'</span>
+<span style="color:#808030; ">&gt;</span> array
+<span style="color:#808030; ">[</span> <span style="color:#800000; ">'</span><span style="color:#cc5555; ">gu</span><span style="color:#800000; ">'</span><span style="color:#808030; ">,</span> <span style="color:#800000; ">'</span><span style="color:#cc5555; ">gu</span><span style="color:#800000; ">'</span><span style="color:#808030; ">,</span> <span style="color:#800000; ">'</span><span style="color:#cc5555; ">ultimo</span><span style="color:#800000; ">'</span> <span style="color:#808030; ">]</span>
+<span style="color:#808030; ">&gt;</span> 
+</pre>
 
 </div>
 </template>
